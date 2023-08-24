f95673b52259093cf46952b3373d8ca78c338378
CiriloMejia <Cirilo Mejia>
2023-08-24 11:01:26 -0500

    Added major

--- hello.c
+++ hello.c
@@ -1,12 +1,15 @@
 /*
-*Printing my name
+ * Author: Cirilo Mejia
+ * Class: CSI
+ * Date: 2023/08/22
+*Printing my name and major
 */
 
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-
+/* This will print my name and my major*/
     printf("!Cirilo Mejia!\n");
     printf("Electrical Engineering\n");
 
