f493963c73c45be11f136c217223c78cf444ab82
kindanotright <mbogatz2@huskers.unl.edu>
2023-08-24 16:14:14 -0500

    initite commit

--- hello.c
+++ hello.c
@@ -13,5 +13,7 @@ int main(int argc, char **argv) {
 /* This will print my name and my major to the screen*/
     printf("!Cirilo Mejia!\n");
     printf("Electrical Engineering\n");
+    printf("Megan Bogatz\n");
+    printf("Computer Engineering!\n");
 /* End of code*/
     return 0;}
