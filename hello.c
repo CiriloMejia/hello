7166853205d89396240daa0c2f812929dd5ec9b1
CiriloMejia <Cirilo Mejia>
2023-08-24 11:10:20 -0500

    Added major

--- hello.c
+++ hello.c
@@ -1,17 +1,17 @@
 /*
  * Author: Cirilo Mejia
  * Class: CSI
- * Date: 2023/08/22
-*Printing my name and major
+ * Date: 2023/08/24
+ * Printing my name and major
 */
 
 /* The start of the code*/
 #include <stdlib.h>
 #include <stdio.h>
-
+/* Starting to print text*/
 int main(int argc, char **argv) {
-/* This will print my name and my major*/
+/* This will print my name and my major to the screen*/
     printf("!Cirilo Mejia!\n");
     printf("Electrical Engineering\n");
-
+/* End of code*/
     return 0;}
